75ff44453db39a817c340faa41d08f0ba6077e96
Auto Logon <autologon@CSED.LAB>
2024-06-27 09:37:23 -0300

    first commit

---
+++ src/Programa.java
@@ -0,0 +1,11 @@
+import controles.Controle;
+
+public class Programa {
+    public static void main(String[] args) throws Exception {
+        Controle controleAluno = new Controle();
+
+        controleAluno.lerAlunos();
+        controleAluno.calcular();
+        controleAluno.gravar();
+    }
+}
---
+++ src/controles/Controle.java
@@ -0,0 +1,51 @@
+package controles;
+import java.util.List;
+
+import daos.GerenciadorArquivos;
+import modelos.Aluno;
+
+public class Controle {
+    private List<Aluno> alunos;
+    private GerenciadorArquivos leitor = new GerenciadorArquivos();
+    private int nAlunos = 0;
+    private int aprovados = 0;
+    private int reprovados = 0;
+    private double menorNota = 10;
+    private double maiorNota = 0;
+    private double soma;
+    private double media;
+
+    public void lerAlunos(){
+        alunos = leitor.lerAlunos();
+    }
+    public void calcular(){
+        this.nAlunos = alunos.size();
+        
+        for (Aluno aluno : alunos) {
+            if(aluno.getNota() >= 6){
+                this.aprovados++;
+            }else{
+                this.reprovados++;
+            }
+
+            if(aluno.getNota() > maiorNota){
+                maiorNota = aluno.getNota();
+            }
+            if(aluno.getNota() < menorNota){
+                menorNota = aluno.getNota();
+            }
+
+            soma += aluno.getNota();
+        }
+
+        this.media = soma/nAlunos;
+        
+    }
+    public void gravar(){
+        if(leitor.guardar(nAlunos, aprovados, reprovados, menorNota, maiorNota, media)){
+            System.out.println("guardado com sucesso");
+        }else{
+            System.out.println("erro ao guardar");
+        }
+    }
+}
